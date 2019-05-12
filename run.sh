cc8485e5093d414c4bc4a1cca1189d787cc76f65
Giulio Bottari <giuliobottari@gmail.com>
2019-05-12 17:46:13 -0300

    Removes requirements.yml and simplifies run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-cd vars
-rm machine_vars.yml
-ln -s `hostname`.yml machine_vars.yml
-cd ..
+ln -nsf "`hostname`.yml" vars/machine_vars.yml
 
-ansible-galaxy install -r requirements.yml
 ansible-playbook playbook.yml -K "$@"
